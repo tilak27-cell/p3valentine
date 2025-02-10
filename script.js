80572f353a7366c1879d0a35ad840eaade756cdc
thdaiuem <dhckgaj13@gmail.com>
2025-02-10 19:37:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Em chắc chưa",
+    "Thực sự chắc chưaaaa???",
+    "Oii nooo, em chắc rùi á?",
+    "Đừng màaaa...",
+    "Bây bê suy nghĩ lại đi",
+    "Em mà hong đi thì anh bùn lắm",
+    "Anh sẽ rất bùn",
+    "Anh sẽ rất rất rất bùn...",
+    "... anh sẽ hong hỏi nữa:<",
+    "Đùa hoiii:< đi chơi với anh nhaaa ❤️"
 ];
 
 let messageIndex = 0;
